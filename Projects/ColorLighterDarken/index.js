cc854e166686c661282ad801f9d51cbc527da917
hiteshjain48 <hiteshjainhd@gmail.com>
2022-12-08 01:14:16 +0530

    Added more functions

--- Projects/ColorLighterDarken/index.js
+++ Projects/ColorLighterDarken/index.js
@@ -13,4 +13,40 @@ hexInput.addEventListener('keyup', () => {
     const stripHex = hex.replace("#","");
     inputColor.style.backgroundColor = "#"+stripHex; 
     console.log(hex);
+});
+
+const convertHexToRGB = (hex) => {
+    if(!isValid(hex)) return null;
+    const stripHex = hex.replace("#","");
+    if(stripHex.length === 3){
+        stripHex = stripHex[0] + stripHex[0] + stripHex[1] + stripHex[1] + stripHex[2] + stripHex[2];
+        const r = parseInt(stripHex.substring(0,2),16);
+        const g = parseInt(stripHex.substring(2,4),16);
+        const b = parseInt(stripHex.substring(4,6),16);
+        return {r,g,b};
+    }
+}
+
+const convertRGBToHex = (r,g,b) => {
+    let rHex = ("0" + r.toString(16)).slice(-2);
+    let gHex = ("0" + g.toString(16)).slice(-2);
+    let bHex = ("0" + b.toString(16)).slice(-2);
+    return rHex+gHex+bHex;
+}
+
+const slider = document.getElementById('slider');
+const sliderText = document.getElementById('sliderText');
+slider.addEventListener('input', () =>{
+    sliderText.textContent = `${slider.value}%`;
+    console.log(slider.value);
 })
+
+const alterColor = (hex, percentage) => {
+    let rgb = convertHexToRGB(hex);
+    rgb.r = Math.floor(rgb.r + 255*percentage/100);
+    rgb.g = Math.floor(rgb.g + 255*percentage/100);
+    rgb.b = Math.floor(rgb.b + 255*percentage/100);
+    return convertRGBToHex(rgb.r,rgb.g,rgb.b);
+}
+
+const alteredColor = document.getElementById("alteredColor");
